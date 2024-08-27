9a21d7261f54e1edb66c25ce620bff56c3d78a37
AlvaroGabrielV <168542138+AlvaroGabrielV@users.noreply.github.com>
2024-08-27 07:58:44 -0300

    Atividade 10.c

---
+++ Atividade 10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+	int main() {
+		
+		int contador, numero = 10;		
+		
+		printf("A contagem regressiva: ");
+		for (contador = 0; contador < numero; numero = numero - 1) {			
+		
+			printf("%d ", numero);			
+		}
+		return 0;				
+	}
