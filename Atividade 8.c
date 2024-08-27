cc1629b8a61897802ecc30d9350b8933185f0b1c
AlvaroGabrielV <168542138+AlvaroGabrielV@users.noreply.github.com>
2024-08-27 07:54:23 -0300

    Atividade 8.c

---
+++ Atividade 8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+	int main() {
+		
+		int numero, resultado, cont;
+		
+		printf("Informe o numero: ");
+		scanf("%d", &numero);
+		
+		for (cont = 0; cont <= 10; cont++) {
+			printf("%d x %d = %d \n", numero, cont, numero * cont);
+    }
+    return 0;
+}
+		
+		
